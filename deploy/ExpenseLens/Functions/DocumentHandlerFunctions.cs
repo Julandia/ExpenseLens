6c09c44cd8718d928664ece0841952d6cecd4203
Julandia <songshanshan@hotmail.com>
2024-06-19 22:39:11 +0200

    Refactor

--- deploy/ExpenseLens/Functions/DocumentHandlerFunctions.cs
+++ deploy/ExpenseLens/Functions/DocumentHandlerFunctions.cs
@@ -1,14 +1,8 @@
-using System.IO;
 using Deploy.ExpenseLens.BackendService;
 using Deploy.ExpenseLens.Shared;
 using Deploy.Infrastructure;
 using Deploy.Infrastructure.Configuration;
-using Pulumi;
-using Pulumi.AzureNative.Authorization;
 using Pulumi.AzureNative.Insights;
-using Pulumi.AzureNative.ManagedIdentity;
-using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
 using Pulumi.AzureNative.Web;
 using Pulumi.AzureNative.Web.Inputs;
 
@@ -16,8 +10,6 @@ namespace Deploy.ExpenseLens.Functions;
 
 public class DocumentHandlerFunctions : IPulumiRegionalResource
 {
-    private const string StorageReaderRoleId = "acdd72a7-3385-48ef-bd42-f606fba81ae7";
-
     public DocumentHandlerFunctions(DeploymentConfig config,
         FunctionResourceNames names,
         DocumentStorageAccount documentStorageAccount,
@@ -45,62 +37,12 @@ public class DocumentHandlerFunctions : IPulumiRegionalResource
             Kind = "web",
         });
 
-        // var storageAccount = new StorageAccount(names.FunctionAppStorageAccountName, new StorageAccountArgs
-        // {
-        //     ResourceGroupName = resourceGroup.Name,
-        //     Sku = new SkuArgs
-        //     {
-        //         Name = SkuName.Standard_LRS,
-        //     },
-        //     Kind = Pulumi.AzureNative.Storage.Kind.StorageV2,
-        //     Location = resourceGroup.Location,
-        // });
-        //
-        // // Create a Blob Container
-        // var codeContainer = new BlobContainer("code-container", new BlobContainerArgs
-        // {
-        //     AccountName = storageAccount.Name,
-        //     ResourceGroupName = resourceGroup.Name,
-        //     PublicAccess = PublicAccess.None
-        // });
-        //
-        // var functionAppPublishFolder = Path.Combine(@"..\..\Publish");
-        //
-        // // Upload ZIP archive to the Blob Container
-        // var zipBlob = new Blob(names.FunctionAppZipContainerName, new BlobArgs
-        // {
-        //     AccountName = storageAccount.Name,
-        //     ResourceGroupName = resourceGroup.Name,
-        //     ContainerName = codeContainer.Name,
-        //     Source = new FileArchive(functionAppPublishFolder),
-        //     Type = BlobType.Block,
-        // });
-
-        // var userIdentity = new UserAssignedIdentity(names.FunctionAppStorageAccessUserIdentityName, new UserAssignedIdentityArgs
-        // {
-        //     ResourceGroupName = resourceGroup.Name,
-        //     Location = resourceGroup.Location,
-        // });
-
-        // var zipRoleAssignment = new RoleAssignment(names.FunctionAppStorageReaderRoleAssignmentName, new RoleAssignmentArgs
-        // {
-        //     PrincipalId = userIdentity.PrincipalId,
-        //     RoleDefinitionId = $"/subscriptions/{config.DeploymentEnvironment.SubscriptionId}/providers/Microsoft.Authorization/roleDefinitions/{StorageReaderRoleId}",
-        //     Scope = storageAccount.Id,
-        //     PrincipalType = "ServicePrincipal",
-        // });
-
         var functionApp = new WebApp(names.FunctionAppName, new WebAppArgs
         {
             Kind = "FunctionApp",
             ResourceGroupName = resourceGroup.Name,
             Location = resourceGroup.Location,
             ServerFarmId = appServicePlan.Id,
-            // Identity = new ManagedServiceIdentityArgs
-            // {
-            //     UserAssignedIdentities = { userIdentity.Id },
-            //     Type = Pulumi.AzureNative.Web.ManagedServiceIdentityType.UserAssigned,
-            // },
             SiteConfig = new SiteConfigArgs
             {
                 AppSettings =
@@ -110,12 +52,6 @@ public class DocumentHandlerFunctions : IPulumiRegionalResource
                         Name = "AzureWebJobsStorage",
                         Value = documentStorageAccount.ConnectionString,
                     },
-                    // new NameValuePairArgs
-                    // {
-                    //     Name = "WEBSITE_RUN_FROM_PACKAGE",
-                    //     //Value = "https://expenselenspulumi.blob.core.windows.net/function/ExpenseLensFunctions-0.0.2.zip",
-                    //     Value = zipBlob.Url,
-                    // },
                     new NameValuePairArgs
                     {
                         Name = "FUNCTIONS_WORKER_RUNTIME",
@@ -159,7 +95,5 @@ public class DocumentHandlerFunctions : IPulumiRegionalResource
                 },
             },
         });
-
-
     }
 }
--- deploy/README.md
+++ deploy/README.md
@@ -1,3 +1,79 @@
-# expenselens-admin
+# ExpensenLens Deployment
+Expenselens services and functions are deployed using Pulumi.
 
-##
+## Pre-requisites
+
+### Resource group (`expenselens-admin`)
+Before deploying to first environment, resource group `expenselens-admin` should be manually created.
+
+The resource group must contain below resources that are shared among deployment environments like `development` and `production`.
+
+### Azure Storage Account (`expenselenspulumi`)
+The storage account is used by Pulumi to store its state files.
+
+Create two containers:
+- `development`
+- `production`
+
+Update storage account name and access key to ExpenseLens repository secrets.
+- `AZURE_STORAGE_ACCOUNT_NAME`: 'expenselenspulumi'
+- `AZURE_STORAGE_ACCOUNT_KEY`: <key>
+
+### Azure Key Vault (`expenselens-pulumi`)
+The key vault is used to store secrets that are used by Pulumi to deploy resources.
+
+Create key: `pulumi-secret-provider`
+
+### Container Registry (`expenselens`)
+The ACR is used to store ExpenseLens backend service container images.
+
+Create repository: `expense-lens-service`
+
+Update ACR secrets to ExpenseLens repository secrets.
+- `ECR_LOGIN_SERVER`: <server>
+- `ECR_USERNAME`: <username>
+- `ECR_PASSWORD`: <password>
+
+## Service Principal (`ExpenseLens github action`)
+The service principal is used by GitHub Actions to authenticate with Azure.
+
+Follow the steps in [this guide](https://learn.microsoft.com/en-us/azure/developer/github/connect-from-azure?tabs=azure-portal%2Clinux) to create a service principal.
+
+For each deployment environment, add credential under Certificates & Secrets -> Federated credentials
+
+See [example of development](docs/github_action_credential_development.jpg)
+
+Update following secrets to ExpenseLens environment secrets.
+- `AZURE_CLIENT_ID`: <client_id>
+- `AZURE_SUBSCRIPTION_ID`: <subscription_id>
+- `AZURE_TENANT_ID`: <tenant_id>
+
+## Pulumi stack
+Create a Pulumi stack for each deployment environment.
+
+Take `development` as an example:
+```pwsh
+cd deploy\ExpenseLens
+az login
+$env:AZURE_STORAGE_ACCOUNT = 'expenselenspulumi'
+$env:AZURE_STORAGE_key = <key>
+pulumi stack init --secrets-provider="azurekeyvault://expenselens-pulumi.vault.azure.net/keys/pulumi-secret-provider"
+```
+
+## Deploy locally (optional)
+```pwsh
+cd deploy\ExpenseLens
+az login
+$env:AZURE_STORAGE_ACCOUNT = 'expenselenspulumi'
+$env:AZURE_STORAGE_key = <key>
+dotnet r preview-dev
+dotnet r deploy-dev
+```
+To deploy Functions, find the FunctionApp name, run
+```pwsh
+cd src\Functions
+az login
+func azure functionapp publish <functionname>
+func azure functionapp publish <functionname> --publish-settings-only
+```
+

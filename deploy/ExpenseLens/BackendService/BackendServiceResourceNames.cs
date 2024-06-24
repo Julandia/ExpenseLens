743936896d0396a4d3ef2de6fbf40271a8e7ba7e
Julandia <songshanshan@hotmail.com>
2024-06-24 22:21:37 +0200

    Correct backend service image tag

--- deploy/ExpenseLens/BackendService/BackendServiceResourceNames.cs
+++ deploy/ExpenseLens/BackendService/BackendServiceResourceNames.cs
@@ -13,7 +13,7 @@ public class BackendServiceResourceNames : ResourceNames
     public string BackendContainerAppName => Resolve("expense-lens-backend-app");
     public string BackendContainerName => Resolve("expense-lens-backend-container");
     public string BackendContainerRepositoryName => Resolve("expense-lens-service");
-    public string BackendContainerImageVersion => System.Environment.GetEnvironmentVariable("IMAGE_VERSION") ?? "37663a75b00a2ecf71fba97c5e5fcc80fb9fd817";
+    public string BackendContainerImageVersion => System.Environment.GetEnvironmentVariable("BACKEND_SERVICE_IMAGE_TAG") ?? "37663a75b00a2ecf71fba97c5e5fcc80fb9fd817";
     public string DocumentIntelligenceServiceName => Resolve("expense-lens-doc-intelligence-service");
 
     public string CosmosDatabaseAccountName => Resolve("expense-lens-db-account");
